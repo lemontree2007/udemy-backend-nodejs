19c9721dcd3cf035bfcacf26e39b5eaede74b2d0
Minh Tri <trilm2007@gmail.com>
2024-11-15 16:25:20 +0700

    config routes

--- src/server.js
+++ src/server.js
@@ -1,22 +1,14 @@
-const express = require("express"); //import express
-const path = require("path");
-const app = express(); // tạo express application
 require("dotenv").config();
+const express = require("express"); //import express
+const configViewEngine = require("./config/viewEngine");
+const webRouter = require("./routes/web");
+const app = express(); // tạo express application
 const port = process.env.PORT || 8888; // init port
 
-//khai báo routes
-//req (request), res(response) là 2 object trong môi trường Node.js
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/sample", (req, res) => {
-  res.render("sample.ejs");
-});
-
-app.set("views", path.join(__dirname, "views")); // khai bao template engine
-app.set("view engine", "ejs");
-
+//config template engine
+configViewEngine(app);
+// config router
+app.use("/", webRouter);
 //run server trên port đã khởi tạo trước đấy
 //nạp các thông tin khai báo ở trên rồi chạy (ví dụ như nạp routes)
 app.listen(port, () => {
